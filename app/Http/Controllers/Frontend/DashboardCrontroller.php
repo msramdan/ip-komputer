507ecf9b2e2bad938c384f821d757a898325dbe7
msramdan <saepulramdan244@gmail.com>
2022-04-17 07:57:50 +0700

    update

--- app/Http/Controllers/Frontend/DashboardCrontroller.php
+++ app/Http/Controllers/Frontend/DashboardCrontroller.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
+
 
 class DashboardCrontroller extends Controller
 {
 
     public function index(Request $request)
     {
-        return view('frontend.index');
+        $Kategori = Kategori::all();
+        return view('frontend.index',[
+            'kategori' => $Kategori
+        ]);
     }
 }
--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -16,6 +16,118 @@ class KategoriSeeder extends Seeder
      */
     public function run()
     {
-        Kategori::factory()->count(5)->create();
+        Kategori::create([
+            'nama_kategori' => 'ALSEYE'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'BLUETOOTH'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'CARDREADER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'CDDVD'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'CH'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'CHARGER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'CONVERTER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'COOLER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'DC'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'DEEPCOOL'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'EARPHONE'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'FAN'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'GAMEPAD'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'GENERIK'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'HDD CADDY'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'HDDCASE'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'KABEL'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'KEYBOARD MOUSE '
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'LASER POINTER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'MB'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'MEJA'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'MIC'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'MODEM'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'MOUSEPAD'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'NETWORKING'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'OTG'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'PASTA PROC'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'PCI CARD'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'PSU'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'REDRAGON'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'SOFTCASE'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'SPEAKER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'SPLITTER'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'SSD'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'TPLINK'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'USBHUB'
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'WEBCAM'
+        ]);
+
+
     }
 }
