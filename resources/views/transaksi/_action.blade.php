af3743ee44ca674c7268a9d91fa3b9305c8e11c2
msramdan <saepulramdan244@gmail.com>
2022-04-30 11:59:31 +0700

    update

--- resources/views/transaksi/_action.blade.php
+++ resources/views/transaksi/_action.blade.php
@@ -200,3 +200,45 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="ajaxModel2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog  modal-xs" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Cetak Laporan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="card shadow mb-4">
+                    <div class="card-body">
+                        <form>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Cetak Semua Transaksi ?</label>
+                            </div>
+                            <br> <br>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Dari Tanggal</label>
+                                <input type="date" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Sampai Tanggal</label>
+                                <input type="date" class="form-control" id="exampleInputPassword1"
+                                    placeholder="Password">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cetak</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
