4fed3276d91c776c7e60c0777681294b0e30f703
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-05-20 19:17:25 +0700

    remove footer

--- resources/views/layouts/_dashboard/footer.blade.php
+++ resources/views/layouts/_dashboard/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Evdigi.my.id {{ date('Y') }}</span>
+            <span>Copyright &copy; {{ date('Y') }}</span>
         </div>
     </div>
 </footer>
